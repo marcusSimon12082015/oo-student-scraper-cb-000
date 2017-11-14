83f7e9147a6184e6e1490178c677edd46d36e027
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-14 19:18:48 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
 
   def initialize(student_hash)
     student_hash.each do |name, value|
-      instance_variable_set(name,value)
+      instance_variable_set("@#{name}",value)
     end
   end
 
